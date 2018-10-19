3212c1cefa828a4d38a57415b12babc40d9e46a1
NsanV <nsanvarela@gmail.com>
2018-10-19 17:14:42 +0200

    alpine

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -24,8 +23,7 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 
 app.use('/', indexRouter);
-app.use('/formAnswer', indexRouter);
-app.use('/users', usersRouter);
+app.use('/index', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
